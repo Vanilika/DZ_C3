44d2651026b9f7c59768cef4e220f3c51746c6b1
valya <vanilik@mail.ru>
2022-12-29 20:50:24 +0300

    решение 23

--- Задача23/Program.cs
+++ Задача23/Program.cs
@@ -1,2 +1,16 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int Qwe(string name)
+{
+    Console.WriteLine(name);
+    int Ax = Convert.ToInt32(Console.ReadLine());
+    return Ax;
+}
 
+int Ax = Qwe("Введи чиcло: ");
+string kub = "";
+
+for( int index = 1; index <= Ax; index++)
+{
+    kub = kub + Math.Pow(index, 3).ToString() + ", ";
+}
+Console.WriteLine(kub);
